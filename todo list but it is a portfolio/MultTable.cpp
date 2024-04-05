935d28cf37da934bab533b11c17737773a3b9b0d
Khanlimon29 <flick1@list.ru>
2024-04-05 23:02:02 +0300

    add stopwatch

--- todo list but it is a portfolio/MultTable.cpp
+++ todo list but it is a portfolio/MultTable.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <conio.h>
 #include "RandNumb.h"
+#include <chrono>
+#include <iomanip>
 using namespace std;
 
 void TableTrainer() {
@@ -12,12 +14,13 @@ void TableTrainer() {
 	int answer = -1;
 	cout << "Выберите количество примеров: ";
 	cin >> quantity;
-	while (cin.fail()) {
+	while (cin.fail() || quantity <= 0) {
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		cout << "Введите значение повторно: ";
 		cin >> quantity;
 	}
+	auto start = chrono::high_resolution_clock::now();
 	for (int i = 0; i < quantity; i++) {
 		a = rng.getRandomInRange(1, 10);
 		b = rng.getRandomInRange(1, 10);
@@ -34,8 +37,20 @@ void TableTrainer() {
 		}
 		cout << "Верно\n";
 	}
+	auto end = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+	auto Avarageduration = duration.count() / quantity;
+
+	int Minutes = duration.count() / 60000;
+	int Seconds = (duration.count() % 60000) / 1000;
+	int Milliseconds = duration.count() % 1000;
+
+	int AvarageSeconds = Avarageduration % 60000 / 1000;
+	int AvarageMilliseconds = Avarageduration % 1000;
+
+	cout << "\nЗатраченное время " << Minutes << ":" << setw(2) << setfill('0') << Seconds << ":" << setw(3) << setfill('0') << Milliseconds << " минут\n";
+	cout << "На один пример в среднем уходит " << AvarageSeconds << "." << setw(3) << setfill('0') << AvarageMilliseconds << " секунд\n";
+
 	cout << "\nНажмите на любую кнопку для продолжения";
 	_getch();
-
-
 }
