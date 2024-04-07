5a3a7f895ea96d54ea663b954ef493bc88e79683
Khanlimon29 <flick1@list.ru>
2024-04-07 23:17:21 +0300

    add new calculator

--- todo list but it is a portfolio/Calc.cpp
+++ todo list but it is a portfolio/Calc.cpp
@@ -1,45 +1,199 @@
-#include <conio.h>
-#include <iostream>
 #include "Calc.h"
+#include <iostream>
+#include <stack>
+#include <string>
+#include <conio.h> 
+#include "setcolor.h"
+
 using namespace std;
 
+int Priority(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+long double ApplyOperation(long double Operand1, long double Operand2, char Op, bool& error) {
+    switch (Op) {
+    case '+': return Operand1 + Operand2;
+    case '-': return Operand1 - Operand2;
+    case '*': return Operand1 * Operand2;
+    case '/':
+        if (Operand2 == 0) {
+            error = true;
+            return 0;
+        }
+        return Operand1 / Operand2;
+    default:
+        error = true;
+        return 0;
+    }
+}
+
+long double EvaluateExpression(const string& Expression, bool& error) {
+    stack<long double> Values;
+    stack<char> Operators;
+
+    for (size_t i = 0; i < Expression.length(); ++i) {
+        if (Expression[i] == ' ')
+            continue;
+
+        if (isdigit(Expression[i]) || Expression[i] == '.') {
+            long double Val = 0;
+            long double Fraction = 0.1;
+            bool DecimalPointFound = false;
+
+            while (i < Expression.length() && (isdigit(Expression[i]) || Expression[i] == '.')) {
+                if (Expression[i] != '.') {
+                    if (!DecimalPointFound) {
+                        Val = Val * 10 + (Expression[i] - '0');
+                    }
+                    else {
+                        Val += (Expression[i] - '0') * Fraction;
+                        Fraction *= 0.1;
+                    }
+                }
+                else {
+                    DecimalPointFound = true;
+                }
+                ++i;
+            }
+            --i;
+            Values.push(Val);
+        }
+        else if (Expression[i] == '(') {
+            Operators.push(Expression[i]);
+        }
+        else if (Expression[i] == ')') {
+            while (!Operators.empty() && Operators.top() != '(') {
+                long double Operand2 = Values.top();
+                Values.pop();
+
+                long double Operand1 = Values.top();
+                Values.pop();
+
+                char Op = Operators.top();
+                Operators.pop();
+
+                Values.push(ApplyOperation(Operand1, Operand2, Op, error));
+                if (error) return 0;
+            }
+            if (!Operators.empty()) Operators.pop();
+        }
+        else {
+            while (!Operators.empty() && Priority(Operators.top()) >= Priority(Expression[i])) {
+                long double Operand2 = Values.top();
+                Values.pop();
+
+                long double Operand1 = Values.top();
+                Values.pop();
+
+                char Op = Operators.top();
+                Operators.pop();
+
+                Values.push(ApplyOperation(Operand1, Operand2, Op, error));
+                if (error) return 0; 
+            }
+            Operators.push(Expression[i]);
+        }
+    }
+
+    while (!Operators.empty()) {
+        long double Operand2 = Values.top();
+        Values.pop();
+
+        long double Operand1 = Values.top();
+        Values.pop();
+
+        char Op = Operators.top();
+        Operators.pop();
+
+        Values.push(ApplyOperation(Operand1, Operand2, Op, error));
+        if (error) return 0; 
+    }
+
+    if (Values.size() != 1) {
+        error = true;
+        return 0;
+    }
+
+    return Values.top();
+}
+
+bool IsOnlySpaces(const string& str) {
+    for (char ch : str) {
+        if (!isspace(ch)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int Calculator() {
-	float a = 0;
-	float b = 0;
-	float devision = 0;
+    string Expression;
+    bool error = false;
 
-	cout << "Введите два числа через пробел: ";
+    cout << "Введите выражение: ";
+    getline(cin, Expression);
 
-	cin >> a;
-	cin >> b;
+    if (Expression.empty() || IsOnlySpaces(Expression)) {
+        SetColor(31);
+        cout << "Пустое выражение!" << endl;
+        SetColor(0);
+        _getch();
+        return 1;
+    }
 
-	while (cin.fail()) {
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "Введите значения повторно: ";
-		cin >> a;
-		cin >> b;
-	}
+    while (!Expression.empty() && Expression.back() == ' ')
+        Expression.pop_back();
 
-	cout << "\nСумма = ";
-	cout << a + b;
+    if (!isdigit(Expression.back()) && Expression.back() != ')') {
+        SetColor(31);
+        cout << "Незавершенное выражение!" << endl;
+        SetColor(0);
+        _getch();
+        return 1;
+    }
 
-	cout << "\nРазность = ";
-	cout << a - b;
+    int OpenBrackets = 0;
+    int ClosedBrackets = 0;
 
-	cout << "\nУмножение = ";
-	cout << a * b;
+    for (char Symbol : Expression) {
+        if (Symbol == '(') {
+            OpenBrackets++;
+        }
+        else if (Symbol == ')') {
+            ClosedBrackets++;
+        }
 
-	if (b != 0) {
-		cout << "\nДеление = ";
-		devision = a / b;
-		cout << devision; 
-	}
-	else { 
-		cout << "\nДеление на ноль невозможно";
-	}
+        if (!isdigit(Symbol) && Symbol != '+' && Symbol != '-' && Symbol != '*' && Symbol != '/' && Symbol != '(' && Symbol != ')' && Symbol != '.' && Symbol != ' ') {
+            SetColor(31);
+            cout << "Некорректные символы в выражении" << endl;
+            SetColor(0); 
+            _getch();
+            return 1;
+        }
+    }
 
-	cout << "\n\nНажмите на любую кнопку для выхода";
-	_getch();
-	return 0;
+    if (OpenBrackets != ClosedBrackets) {
+        SetColor(31);
+        cout << "Незакрытые скобки в выражении!" << endl;
+        SetColor(0);
+        _getch();
+        return 1;
+    }
+
+    long double result = EvaluateExpression(Expression, error);
+    if (error) {
+        SetColor(31);
+        cout << "Некорректное выражение!" << endl;
+        SetColor(0);
+    }
+    else {
+        cout << "Результат: " << result << endl;
+    }
+
+    cout << "\nНажмите на любую кнопку для продолжения";
+    _getch();
+    return 0;
 }
